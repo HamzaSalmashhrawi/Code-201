d865b02186395804b0c8cb7884d93e8a65e6e5b8
HamzaSalmashhrawi <hamza.almshhrawi1997@gmail.com>
2021-04-25 14:43:33 +0300

    class01

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+this is a comment in the js file 
+
+var variable
+
+let 
+const
+*/
+
+// const I can declare it and use it only one time
+// const url = 'https://github.com/';
+// console.log(url);
+
+// const url = 'baraa\'h';
+
+
+let userName = prompt('what\'s your name');
+console.log(userName);
+
+
+let color = prompt('what\'s your fav color');
+console.log(color);
+
+let movie = prompt('how do describe your level in coding from 1 to 10');
+console.log(movie)
+
+let age = prompt('whats your age');
+console.log(age);
+
+alert('Hello ' + userName + ' your fav color is ' + color + ' and your age is ' + age);
